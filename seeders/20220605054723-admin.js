f01cc1bd42d3b0751c5ea82b69548e9ce924f4db
ToEzBit <jirayut52328172@hotmail.com>
2022-06-05 12:53:33 +0700

    feat : model admin

---
+++ seeders/20220605054723-admin.js
@@ -0,0 +1,34 @@
+"use strict";
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert("Admins", [
+      {
+        userName: "admin",
+        password: "admin",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+  },
+
+  async down(queryInterface, Sequelize) {
+    return queryInterface.bulkDelete("Admins", null, {});
+
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  },
+};
