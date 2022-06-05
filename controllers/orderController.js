ddd9ee41aa8e8fb9cf58c2f21e6beea8650aa95d
ToEzBit <jirayut52328172@hotmail.com>
2022-06-06 02:16:27 +0700

    feat : create order

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,13 +1,25 @@
-const { Product } = require("../models");
+const { Product, Order, OrderProduct } = require("../models");
 const createError = require("../utils/createError");
 exports.createOrder = async (req, res, next) => {
   try {
+    const { id } = req.user;
     const { productId, amount } = req.body;
 
-    const product = await Product.findOne({ id: productId });
+    const product = await Product.findOne({ where: { id: productId } });
     if (!product) {
-      createError(400, "Product not found");
+      createError("Product not found", 400);
     }
+    const order = await Order.create({ userId: id, status: "pending" });
+    const orderProduct = await OrderProduct.create({
+      orderId: order.id,
+      productId,
+      amount,
+      price: product.price * amount,
+    });
+
+    order.totalPrice = product.price * amount;
+    order.save();
+    res.json({ order, orderProduct });
   } catch (err) {
     next(err);
   }
