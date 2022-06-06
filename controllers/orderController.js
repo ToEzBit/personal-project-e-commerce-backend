eb436a6901788875966bf71dd2023ed341460856
ToEzBit <jirayut52328172@hotmail.com>
2022-06-06 10:52:33 +0700

    feat : add order product

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -9,17 +9,48 @@ exports.createOrder = async (req, res, next) => {
     if (!product) {
       createError("Product not found", 400);
     }
-    const order = await Order.create({ userId: id, status: "pending" });
+    const order = await Order.create({ userId: id, status: "neworder" });
     const orderProduct = await OrderProduct.create({
       orderId: order.id,
       productId,
       amount,
-      price: product.price * amount,
+      price: product.price,
     });
 
     order.totalPrice = product.price * amount;
     order.save();
     res.json({ order, orderProduct });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.addOrderProduct = async (req, res, next) => {
+  try {
+    const { orderId } = req.params;
+    const { productId, amount } = req.body;
+
+    const order = await Order.findOne({ where: { id: orderId } });
+    if (!order) {
+      createError("Order not found", 400);
+    }
+
+    const product = await Product.findOne({ where: { id: productId } });
+    if (!product) {
+      createError("Product not found", 400);
+    }
+
+    const createdOrderProduct = await OrderProduct.create({
+      orderId: order.id,
+      productId,
+      amount,
+      price: product.price,
+    });
+
+    order.totalPrice += product.price * amount;
+    await order.save();
+
+    res.json({ order, createdOrderProduct });
   } catch (err) {
     next(err);
   }
--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -3,4 +3,6 @@ const router = express.Router();
 const orderController = require("../controllers/orderController");
 
 router.post("/", orderController.createOrder);
+router.post("/:orderId", orderController.addOrderProduct);
+
 module.exports = router;
